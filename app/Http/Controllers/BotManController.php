40c827c2ae02b277b818bd111295601ae00933c2
HTOO MAUNG THAIT <htoo.mt@blueplanet.com.mm>
2020-10-29 00:23:45 +0630

    verify service update #2

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -14,6 +14,7 @@ class BotManController extends Controller
     public function handle()
     {
         $botman = app('botman');
+        $botman->verifyServices('I_SAID_HAHA');
 
         $botman->listen();
     }
