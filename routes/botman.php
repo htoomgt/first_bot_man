2b8d7753a3e03cbadf6e9e5851c9a02cf64e9385
HTOO MAUNG THAIT <htoo.mt@blueplanet.com.mm>
2020-10-29 00:02:47 +0630

    get started action added

--- routes/botman.php
+++ routes/botman.php
@@ -6,4 +6,9 @@ $botman = resolve('botman');
 $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
+
+$botman->hear('GET_STARTED', function($bot){
+    $bot->reply("I am bot and you get started the service");
+});
+
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
