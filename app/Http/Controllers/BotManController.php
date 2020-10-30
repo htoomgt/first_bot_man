97ded9028a9dca6cf9a903555bdc8c441d79cc60
HTOO MAUNG THAIT <htoo.mt@blueplanet.com.mm>
2020-10-30 11:33:41 +0630

    button type chaged to test fix#2.2

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -46,24 +46,7 @@ class BotManController extends Controller
     }
 
     public function messengerGetStarted(BotMan $bot){
-        $question = Question::create("Huh - you woke me up. What do you need?")
-            ->fallback('Unable to ask question')
-            ->callbackId('ask_reason')
-            ->addButtons([
-                Button::create('Tell a joke')->value('joke'),
-                Button::create('Give me a fancy quote')->value('quote'),
-            ]);
-
-        return $this->ask($question, function (Answer $answer) {
-            if ($answer->isInteractiveMessageReply()) {
-                if ($answer->getValue() === 'joke') {
-                    $joke = json_decode(file_get_contents('http://api.icndb.com/jokes/random'));
-                    $this->say($joke->value->joke);
-                } else {
-                    $this->say(Inspiring::quote());
-                }
-            }
-        });
+        $bot->reply("Hello, let's start conversation");
 
     }
 
